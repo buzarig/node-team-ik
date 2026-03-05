44b39f95634977d555fb62fa9511f4f9389020c0
Kimmimaru <kseniasereda12.01@gmail.com>
2026-03-05 19:30:21 +0200

    small fixes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,10 +4,8 @@ const homeController = require('../controllers/home.controller');
 
 const router = express.Router();
 
-// Главная (рендер EJS через контроллер)
 router.get('/', homeController.renderHome);
 
-// Статическая HTML страница (.html) — требование 2 лабы
 router.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'pages', 'about.html'));
 });
--- src/routes/team.routes.js
+++ src/routes/team.routes.js
@@ -3,10 +3,8 @@ const teamController = require('../controllers/team.controller');
 
 const router = express.Router();
 
-// Страница команды — динамическая, данные из JSON
 router.get('/team', teamController.showTeam);
 
-// Динамическая страница участника
 router.get('/member/:id', teamController.showMember);
 
 module.exports = router;
