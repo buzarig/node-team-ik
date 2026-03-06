eded2ba186a302690c9daafb9f00ab76a6cce27a
Kimmimaru <kseniasereda12.01@gmail.com>
2026-03-06 09:15:17 +0200

    https

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ---- View engine (EJS) ----
-app.set('views', path.join(__dirname, 'views')); // папку views создадут другие
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // ---- Static files ----
-app.use(express.static(path.join(__dirname, 'public'))); // папку public создадут другие
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ---- Routes ----
 app.use('/', indexRoutes);
